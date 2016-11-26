34be8258af47e3c5c332ff8696f1c2502649af59
kmustahsan <kashif7@vt.edu>
2016-11-25 20:22:26 -0500

    Created Mirage and seeded data.

--- mirage/factories/users.js
+++ mirage/factories/users.js
@@ -1,4 +1,11 @@
-import { Factory } from 'ember-cli-mirage';
+import Mirage, {faker}  from 'ember-cli-mirage';
 
-export default Factory.extend({
+export default Mirage.Factory.extend({  
+   first_name: faker.name.firstName,
+    last_name: faker.name.lastName,
+    email: faker.internet.email,
+    description:faker.lorem.sentence,
+    username: faker.internet.userName,
+    password: faker.internet.password,
+    title: faker.hacker.phrase
 });
