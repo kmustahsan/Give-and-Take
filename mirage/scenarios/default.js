2588e2b4b9393f6e8978f69c45dc3e079fcd76f8
tav0 <lv23@vt.edu>
2016-11-27 12:22:47 -0500

    fix user and service relationship

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,5 +1,6 @@
 export default function(server ) {
     server.createList('user', 5);
-    server.createList('service', 5);
+    let user = server.create('user');
+    server.createList('service', 5, {'userId': user.id});
     server.loadFixtures();
 }
